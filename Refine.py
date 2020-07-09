078ab1439cc425d77285683ad1290e02167c0c57
Benjlr <roberts.benjamin.luke@gmail.com>
2020-07-09 12:52:47 +1000

    updates

--- Refine.py
+++ Refine.py
@@ -5,10 +5,11 @@ import parameters
 
 
 def refine_registration(source, target, globalTrans):
-    estimate_normals(source, KDTreeSearchParamHybrid(radius=parameters.normal_search_radius, max_nn=30)) #30
-    estimate_normals(target, KDTreeSearchParamHybrid(radius=parameters.normal_search_radius, max_nn=30)) #30
-    result = registration_icp(source, target, parameters.local_icp_distance_threshold, globalTrans, TransformationEstimationPointToPlane())
-    Common.draw_registration_result(source, target, result.transformation)
+    #estimate_normals(source, KDTreeSearchParamHybrid(radius=parameters.normal_search_radius, max_nn=30)) #30
+    #estimate_normals(target, KDTreeSearchParamHybrid(radius=parameters.normal_search_radius, max_nn=30)) #30
+    #Common.draw_registration_result(source, target, np.identity(4))
+    result = registration_icp(source, target, parameters.local_icp_distance_threshold, globalTrans, TransformationEstimationPointToPoint())
+    #Common.draw_registration_result(source, target, result.transformation)
     return result
 
 
--- parameters.py
+++ parameters.py
@@ -1,7 +1,20 @@
-voxel_size = 2 #0.5 #2
-normal_search_radius = 3 #1 #3
-feature_search_radius = 2.5 #2.5
-local_icp_distance_threshold = 0.5
-global_icp_distance_threshold = 0.75
-global_icp_repeat = 40 #5  #40
-ransac_feature_match_threshold = 50000 #10000 #50000
+c = 1
+
+if c == 1:
+    voxel_size = 0.5 #0.5 #2
+    normal_search_radius = 1 #1 #3
+    feature_search_radius = 4 #2.5
+    local_icp_distance_threshold =15
+    global_icp_distance_threshold = 2
+    global_icp_repeat = 1 #5  #40
+    ransac_feature_match_threshold = 10000 #10000 #50000
+
+
+if c == 2:
+    voxel_size = 3 #0.5 #2
+    normal_search_radius = 3 #1 #3
+    feature_search_radius = 3#2.5
+    local_icp_distance_threshold = 0.1
+    global_icp_distance_threshold = 0.75
+    global_icp_repeat = 30 #5  #40
+    ransac_feature_match_threshold = 100000 #10000 #50000
